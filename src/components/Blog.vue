76e01d227c9a7d8784e16fbe667635a47536f03c
manuelmejiaio <mejiamanuel57@gmail.com>
2024-02-23 22:13:09 -0500

    blog order

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -43,12 +43,6 @@ export default {
           imgSource: `https://1.bp.blogspot.com/-36fex_hwYR0/XygHcLwE8wI/AAAAAAAAZaY/epOxwihtSb8gcTVOKd7HftsI5zpczzQmACLcBGAsYHQ/w400-h267/manuelmejiajr.com%2Baccessibility.jpg`
         },
         {
-          title: `Key people to follow in the Vue world`,
-          content: `For me, one of the small hacks to keep me posted with the tech/tools that I love is to follow the contributors or creators on Twitter. I like to read about what they're thinking, what they're working on and what are the next improvements for the tools. I even like to see their favorite memes 😆. So I encourage you to start …`,
-          link: `https://blog.manuelmejia.io.com/2020/01/key-people-to-follow-in-vue-world.html`,
-          imgSource: `https://1.bp.blogspot.com/-QgBlURd_LGc/XiiCZ36b1aI/AAAAAAAAWOo/cj0bkIGaTx4d_5pHaLqTmKvsgNT78Wx-gCLcBGAsYHQ/s320/vue%2Bkey%2Bpeople.png`
-        },
-        {
           title: `Clean Code, even under pressure`,
           content: `We all have been in the position at work when the pressure comes, and delivering on time is a "life-or-death situation". There are a lot of factors that influence a bad project planning like wrong time estimation, workload distribution, team choice, etc...and maybe it will happen more than once, after all, we are humans an…`,
           link: `https://blog.manuelmejia.io.com/2018/07/clean-code-even-under-pressure.html`,
@@ -59,6 +53,12 @@ export default {
           content: `Maybe, the first thing that comes to your mind when you hear the word "KATA" is a movie of Bruce Lee or maybe, the movie Kung Fu Panda 😛. According to Wikipedia Kata is: "Japanese word, are detailed choreographed patterns of movements practiced either solo or in pairs. The term form is used for the corresponding concept in…`,
           link: `https://blog.manuelmejia.io.com/2017/09/improve-your-skills-as-developer-with.html`,
           imgSource: `https://1.bp.blogspot.com/-dFL5J1ifksE/Was7xmspKfI/AAAAAAAAKgU/3s6rg3w9KiofhxuoRnmdrwkdpdyyHgsmQCK4BGAYYCw/s320/tumblr_nnf33pZFzt1qfcfclo1_500.gif`
+        },
+        {
+          title: `Key people to follow in the Vue world`,
+          content: `For me, one of the small hacks to keep me posted with the tech/tools that I love is to follow the contributors or creators on Twitter. I like to read about what they're thinking, what they're working on and what are the next improvements for the tools. I even like to see their favorite memes 😆. So I encourage you to start …`,
+          link: `https://blog.manuelmejia.io.com/2020/01/key-people-to-follow-in-vue-world.html`,
+          imgSource: `https://1.bp.blogspot.com/-QgBlURd_LGc/XiiCZ36b1aI/AAAAAAAAWOo/cj0bkIGaTx4d_5pHaLqTmKvsgNT78Wx-gCLcBGAsYHQ/s320/vue%2Bkey%2Bpeople.png`
         }
       ]
     }
