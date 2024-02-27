4ac6c21bca9d573be18a4b0d0d5681cd706c9573
manuelmejiaio <mejiamanuel57@gmail.com>
2024-02-27 11:38:47 -0500

    updating bio

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,11 +6,12 @@
     <img alt="Manuel Mejia" class="h-32 md:h-64 mx-auto rounded-full" src="@/assets/images/photo.jpg" />
     <p class="text-4xl md:text-6xl">Hello, I'm Manuel.</p>
     <p class="md:w-9/12 text-xl md:text-2xl mb-10">
-      I’m a Software Engineer currently working as Tech Lead at
+      A Software Engineer with over a decade of experience in developing web applications. Currently, I perform my role
+      as Tech Lead at
       <a class="font-bold hover:text-green-400" target="_blank" rel="noopener" href="https://www.buildinglink.com/">
         BuildingLink</a
-      >. I like to work on new projects following good practices and using top-notch technologies. In my spare time, I
-      enjoy sharing what I learn on my blog. Also, I'm a co-host of a dev podcast,
+      >. I am passionate about working on products that adhere to the highest standards and utilizing top-notch
+      technologies. I enjoy sharing my knowledge through my blog and co-hosting the developer-focused podcast,
       <a class="font-bold hover:text-green-400" target="_blank" rel="noopener" href="https://linktr.ee/ligadelcodigo"
         >@LigaDelCodigo</a
       >.
